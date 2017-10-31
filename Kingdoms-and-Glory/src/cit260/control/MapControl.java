0921083c31c6c2ceb1b7e231385985c55a43d3e2
bclegg <bradclegg@10.50.73.42>
2017-10-31 13:09:49 -0600

    Moved DetermineWinOrLose into MapControl

--- Kingdoms-and-Glory/src/cit260/control/MapControl.java
+++ Kingdoms-and-Glory/src/cit260/control/MapControl.java
@@ -31,4 +31,45 @@ public class MapControl {
 
         return finalAmount;
     }
+    
+    public static String determineWinOrLose(int leaderValue, int armyValue, int resourceBonus, int cityValue, int enemyLeaderValue, int enemyResourceBonus) {
+        
+        if (leaderValue > 5 || leaderValue < 1) {
+
+            return "error";
+        }
+        else if (armyValue > 10 || armyValue < 1) {
+	
+            return "error";
+        }
+
+        else if (resourceBonus > 1 || resourceBonus < 0) {
+            
+            return "error";
+        }   
+
+        else if (cityValue > 10 || cityValue < 1) {
+	
+            return "error";
+        }
+        
+
+        else if (enemyLeaderValue > 5 || enemyLeaderValue < 1) {
+
+            return "error";
+        
+        }
+        
+        int attackValue = (leaderValue + armyValue) + (resourceBonus); 
+        int cityDefenseValue = (cityValue + enemyLeaderValue) + (enemyResourceBonus);
+
+        if (attackValue > cityDefenseValue) {
+            
+            return "Win";
+        }
+        else {
+
+            return "Lose";
+        }
+    }
 }

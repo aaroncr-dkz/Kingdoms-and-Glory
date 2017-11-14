d5db7810ea5734575c4f8a80ffbab89319f0f819
bclegg <bradclegg@10.50.73.42>
2017-11-14 13:48:14 -0700

    Created SceneEnum

---
+++ Kingdoms-and-Glory/src/cit260/model/SceneEnum.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260.model;
+
+/**
+ *
+ * @author bradclegg
+ */
+public enum SceneEnum {
+    CoW_Captured,
+    BoS_Examine,
+    SD_Attack,
+    SD_Captured,
+    LJ_Examine,
+    PF_Examine,
+    DG_Exmine,
+    SoZ_Attack,
+    SoZ_Captured,
+    IA_Examine,
+    KP_Examine,
+    KS_Examine,
+    D_Attack,
+    D_Captured,
+    BR_Examine,
+    VoK_Examine,
+    CoM_Attack,
+    CoM_Captured,
+    Ana_Attack,
+    Ana_Captured,
+    Ant_Attack,
+    Ant_Captured,
+    TT_Examine,
+    FA_Attack,
+    FA_Captured,
+    RS_Examine,
+    SoK_Examine,
+    DD_Examine,
+    FS_Attack,
+    FS_Captured,
+    X_Attack,
+    X_Captured,
+    Z_Attack,
+    Z_Captured,
+    TS_Attack,
+    TS_Captured;
+}

e8a194a89745c8e3975e4f4fd2f513a2187e034d
bclegg <bradclegg@10.50.73.42>
2017-11-14 21:20:09 -0700

    Created viewMap function. (draft)

--- Kingdoms-and-Glory/src/cit260/view/GameMenuView.java
+++ Kingdoms-and-Glory/src/cit260/view/GameMenuView.java
@@ -5,6 +5,11 @@
  */
 package cit260.view;
 
+import cit260.model.Game;
+import cit260.model.Map;
+import cit260.model.Territory;
+import kingdoms.and.glory.KingdomsAndGlory;
+
 /**
  *
  * @author bradclegg
@@ -76,8 +81,40 @@ public class GameMenuView extends View {
     }
 
     private void viewMap() {
-        
+       Game game = new Game();
+       
+       KingdomsAndGlory.getCurrentGame();
+       
+       game.getMap();
+       
+       Map map = new Map();
+       map.getMap();
+       
+       System.out.println("-----Zenobia-----");
+       System.out.println("1   2   3   4   5");
+       
+       int numOfRows = 5;
+       int numOfColumns = 5;
+       int k = 1;
+       int row = k++;
+       Territory[][] territorysInMap = new Territory[numOfRows][numOfColumns];
+       
+               
+       for (int i = 0; i < territorysInMap.length; i++) {
+           System.out.println("-----------------");
+           System.out.println("\n" + row);
+           
+           for (int j = 0; j < territorysInMap[i].length; j++) {
+               System.out.println("|");
+               if (territorysInMap.equals(true)) {
+                   System.out.print(" XX ");
+               }
+               else {
+                   System.out.print(" ?? ");
+               }
+            }
+           System.out.print("|");
+        }
+       System.out.print("-----------------");
     }
-    
-    
 }

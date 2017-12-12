9273adfb5b8e13383d5b4a2d7d1de13eee116039
zoma <aaroncr-dkz@users.noreply.github.com>
2017-12-12 13:37:05 -0700

    added VictoryScreenView class

--- Kingdoms-and-Glory/src/cit260/view/VictoryScreenView.java
+++ Kingdoms-and-Glory/src/cit260/view/VictoryScreenView.java
@@ -9,32 +9,13 @@ package cit260.view;
  *
  * @author bradclegg
  */
-public class VictoryScreenView extends View {
+public class VictoryScreenView {
     
     public VictoryScreenView() {
         
     }
 
-    @Override
-    public String[] getInputs() {
-
-        String[] inputs = new String[1];
-        
-        // display menu
-        
-        
-        inputs[0] = this.getInput("Select item from menu by entering the appropriate letter: ");
-        return inputs;
-    }
-
-    @Override
-    public Boolean doAction(String[] inputs) {
-
-        String command = inputs[0].toUpperCase();
-
-        switch (command) {
-           
-        }
-        return false;
+    public void display() {
+        System.out.println("YOU WIN");
     }
 }

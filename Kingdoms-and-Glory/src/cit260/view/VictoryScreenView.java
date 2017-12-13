45ece5e9a0009a2bb4f14e67424aa7385d173f2e
zoma <aaroncr-dkz@users.noreply.github.com>
2017-12-13 13:37:37 -0700

    Fleshed out the victory screen

--- Kingdoms-and-Glory/src/cit260/view/VictoryScreenView.java
+++ Kingdoms-and-Glory/src/cit260/view/VictoryScreenView.java
@@ -16,6 +16,41 @@ public class VictoryScreenView {
     }
 
     public void display() {
-        System.out.println("YOU WIN");
+        System.out.println("\n====================================================\n"
+                         + "* So fate cannot be defeated after all...          *\n"
+                         + "*                                                  *\n"
+                         + "* Ah, can you hear it? Hear the beat of the drums  *\n"
+                         + "* of the netherworld? I was so close...            *\n"
+                         + "*                                                  *\n"
+                         + "* You will see for yourself one day. The power of  *\n"
+                         + "* Demundza, lord of the Ogres...                   *\n"
+                         + "====================================================\n");
+        
+        System.out.println("\n====================================================\n"
+                         + "* Festivities were held all across the kingdom to  *\n"
+                         + "* thier newfound freedom, and the capital a grand  *\n"
+                         + "* party celebrating all the brave soldiers who     *\n"
+                         + "* fought for their King.                           *\n"
+                         + "*                                                  *\n"
+                         + "* However, in the middle of the party, several     *\n"
+                         + "* heroes and their friends headed again for the    *\n"
+                         + "* wilderness. But several men and women watched    *\n"
+                         + "* that depature through a crystal orb...           *\n"
+                         + "*                                                  *\n"
+                         + "* A new journey was just starting. Those heroes    *\n"
+                         + "* could have had no idea what fate would deal      *\n"
+                         + "* them. The battle has just begun... Until that    *\n"
+                         + "* time, enjoy yourselves as you can. We're look-   *\n"
+                         + "* ing forward to the day you appear before us.     *\n"
+                         + "* Until then, may the gods smile upon the heroes.  *\n"
+                         + "====================================================\n");
+        
+        System.out.println("                Kingdoms and Glory\n"
+                         + "                     made by:\n"
+                         + "\n"
+                         + "                   Aaron Reekie\n"
+                         + "                and Brad Clegg\n"
+                         + "\n"
+                         + "               Thanks for playing!\n");
     }
 }
